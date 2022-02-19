bea5fc30e9e645b157eebb5d5fa875312933ecb7
netwave8888 <taoxu8888@gmail.com>
2022-02-19 14:32:06 -0500

    Initial commit

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,79 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CurrencyExchange {
+	@Id
+	private Long id;
+	@Column(name="currency_from")
+	private String from;
+	@Column(name="currency_to")
+	private String to;
+	private BigDecimal conversionMultiple;
+	private String env;
+	
+	public CurrencyExchange() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple, String env) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+		this.env = env;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public String getEnv() {
+		return env;
+	}
+
+	public void setEnv(String env) {
+		this.env = env;
+	}
+	
+	
+	
+	
+	
+	
+}
